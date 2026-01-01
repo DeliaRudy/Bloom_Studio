7cffe6d53c42175baed7502b18e8752da5cd99c7
Ruvimbo Delia HAKATA <hakatar@africau.edu>
2026-01-01 17:20:05 +0000

    Try fixing this error: `Console Error: React does not recognize the `asC

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -415,10 +415,11 @@ SidebarContent.displayName = "SidebarContent"
 
 const SidebarGroup = React.forwardRef<
   HTMLDivElement,
-  React.ComponentProps<"div">
->(({ className, ...props }, ref) => {
+  React.ComponentProps<"div"> & { asChild?: boolean }
+>(({ className, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "div"
   return (
-    <div
+    <Comp
       ref={ref}
       data-sidebar="group"
       className={cn("relative flex w-full min-w-0 flex-col p-2", className)}
