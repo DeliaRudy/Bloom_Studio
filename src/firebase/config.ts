a001fe9009f86935148db6eb0d54467c96b2b963
Ruvimbo Delia HAKATA <hakatar@africau.edu>
2026-01-01 18:33:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7201999273-4b18b",
+  "appId": "1:339694204604:web:dc85e1d0b3d8c54e6bc2b9",
+  "apiKey": "AIzaSyAff4S0TiJ0kNPUST1tvP_Nwffd4N-38TI",
+  "authDomain": "studio-7201999273-4b18b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "339694204604"
+};
